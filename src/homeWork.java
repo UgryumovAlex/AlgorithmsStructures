5e4261d9140d01d2641d54a416205038e04c2a50
UgryumovAlex <ugryumov_alex@mail.ru>
2021-06-18 16:10:32 +0300

    Домашнее задание №8. Хэш-таблицы.

--- src/homeWork.java
+++ src/homeWork.java
@@ -1,5 +1,28 @@
+/**
+ * 1. Создать программу, реализующую метод цепочек.
+ *
+ * */
+
 public class homeWork {
     public static void main(String[] args) {
-        System.out.println("Курс Алгоритмы и Структуры данных на JAVA");
+        HashTable<Product, Integer> hashTable = new HashTableImpl<>(10);
+
+        hashTable.put(new Product(1, "Orange"), 150);
+        hashTable.put(new Product(77, "Banana"), 100);
+        hashTable.put(new Product(67, "Carrot"), 220);
+        hashTable.put(new Product(62, "Lemon"), 250);
+        hashTable.put(new Product(52, "Tomato"), 120);
+        hashTable.put(new Product(24, "Potato"), 67);
+        hashTable.put(new Product(44, "Pineapple"), 228);
+
+        hashTable.display();
+
+        System.out.println("Стоимость морковки : " + hashTable.get(new Product(67, "Carrot")));
+        System.out.println("Стоимость ананаса  : " + hashTable.get(new Product(44, "Pineapple")));
+
+        Integer price = hashTable.remove(new Product(24, "Potato"));
+        System.out.println("Удалили картошку стоимостью " + price);
+
+        hashTable.display();
     }
 }
